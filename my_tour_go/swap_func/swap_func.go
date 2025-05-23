54180c500f1d64745c0ed9fd2e429dad374fae0c
serhii <shevardino1@gmail.com>
2025-05-23 16:56:42 +0300

    add my_swap

--- my_tour_go/swap_func/swap_func.go
+++ my_tour_go/swap_func/swap_func.go
@@ -6,7 +6,12 @@ func swap(x, y string) (string, string) {
 	return y, x
 }
 
+func my_swap(x, y, z string) (string, string, string) {
+	return z, y, x
+}
 func main() {
 	a, b := swap("Serhii", "Hello")
 	fmt.Println(a, b)
+	a, b, c := my_swap("test", "first", "My")
+	fmt.Println(a, b, c)
 }
