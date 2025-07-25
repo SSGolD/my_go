1b1b50b027db9b8c63635150b1b39231856107bb
serhii <shevardino1@gmail.com>
2025-07-25 17:23:01 +0300

    les_02

---
+++ myсomputerScienceGo/lesson02-variables/lesson02-variables.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "Serhii" 
+	age := 33
+	weight := 105.5
+	training := true
+	fmt.Println("Name:", name, "| Age:", age , "| Weight:", weight, "| Training:", training)
+}
