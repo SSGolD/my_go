047d2cab57ed0b65f870529131e473d8904b13ad
serhii <shevardino1@gmail.com>
2025-10-09 00:29:19 +0300

    change in les04_for

--- myсomputerScienceGo/les04_for/les04_for.go
+++ myсomputerScienceGo/les04_for/les04_for.go
@@ -6,4 +6,13 @@ import "fmt"
 	for i := 0; i < 5; i++ {
 		fmt.Println("SerJ")
 	}
+	for i := 10; i > 0; i-- {
+		fmt.Println(i)
+	}
+	s := 0 // sum 1 to 100 
+	for i := 0; i < 101; i++ {
+	    s += i;
+	}
+	fmt.Println(s)
  }
+ 
