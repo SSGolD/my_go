40cbfd669a9f2e35bb42d3024c0fdc7374f02907
serhii <shevardino1@gmail.com>
2026-04-28 16:44:40 +0300

    even_numbers_from_list

---
+++ 2026_gemini_go/even_numbers_from_list/even_numbers_from_list.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+ 
+    numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+    fmt.Println("Парні числа зі списку:")
+
+
+    for _, value := range numbers {
+        
+     
+        if value % 2 == 0 {
+         
+            fmt.Println(value)
+        }
+    }
+}
